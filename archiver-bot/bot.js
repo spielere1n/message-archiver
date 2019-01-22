e0181387757e335169688ed1a7312decea45bdb6
A.C. Miller <acmiller03@gmail.com>
2019-01-22 08:40:01 -0800

    ...........

--- archiver-bot/bot.js
+++ archiver-bot/bot.js
@@ -18,27 +18,32 @@ bot.on('message', (message) => {
     const channel = bot.channels.get('537131544821628929');
     const args = message.content.slice(config.prefix.length).split(' ');
     const command = args.shift().toLowerCase();
-    let msg = new Msg(message.content);
 
     if(command === 'archive') {
         channel.fetchMessage(args[0])
-        msg.save()
-            .then(message =>  {
-                //Promise.resolve(message)
+            .then(fetchedMessage =>  {
+                /*message is already a resolved promise, why are you resolving it again?
+                also, don't use the same name in your then event as in the message event
+                change it to msg or fetchedMessage or something else and then do everything, 
+                including saving the content to your db, inside the then event*/
+                let msg = new Msg(message.content);
+                msg.save((err) => {
+                    if(err) {
+                        return handleError(err);
+                    } else {
+                        console.log('success');
+                    }
+                });
+
                 console.log(message.content);
             })
             .catch(console.error);
         //let json = JSON.parse(dm);
         //console.log(json);
         //Promise.resolve(dm);
+       
         
-        /*msg.save()
-            .then(json => {
-                console.log('success');
-            })
-            .catch(err => {
-                console.log(err);
-            });*/
+        
 
         message.reply('Message successfully archived');
     }
